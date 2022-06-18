9ac34136c2735db00641f5315f5e8a529cde39fc
xlostpexz <98389349+xlostpexz@users.noreply.github.com>
2022-06-18 12:31:35 +0700

    Update yedtud.lua

--- yedtud.lua
+++ yedtud.lua
@@ -840,7 +840,7 @@ end
         pcall(function()
             if _G.Raid then
 for i,v in pairs(game:GetService("Workspace").MOB:GetChildren()) do
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.HumanoidRootPart.CFrame * CFrame.new(0,-5,10)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.HumanoidRootPart.CFrame * CFrame.new(0,-5,9)
     end
             end
         end)
